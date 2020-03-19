12e94464a2ae03fc8fb1c1a86e76b44ac108446a
Faiz Mokhtar <mfaizmokhtar@airasia.com>
2020-03-19 20:28:39 +0800

    initial commit

---
+++ JanjiXkcd/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  JanjiXkcd
+//
+//  Created by Faiz Mokhtar on 19/03/2020.
+//  Copyright © 2020 Faiz Mokhtar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
